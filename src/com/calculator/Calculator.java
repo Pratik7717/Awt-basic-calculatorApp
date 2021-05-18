c847fdd5264a88333f2f4154b4846e81f3f11966
Pratik Jagade <pratik.21810767@viit.ac.in>
2021-05-18 12:59:12 +0530

    Updates Calculation.java..added required components in the panel of frame

--- src/com/calculator/Calculator.java
+++ src/com/calculator/Calculator.java
@@ -15,5 +15,43 @@ public class Calculator {
 		frm=new Frame("Basic Calculator");
 		frm.setSize(400,400);
 		frm.setVisible(true);
+		
+		pnl=new Panel();
+		frm.add(pnl);
+		
+		GridLayout gl=new GridLayout(6, 2);
+		pnl.setLayout(gl);
+		
+		lno1=new Label("Enter First Number : ");
+		lno2=new Label("Enter second number : ");
+		lans=new Label("Answer : ");
+		
+		tno1=new TextField();
+		tno2=new TextField();
+		tans=new TextField();
+		
+		btnadd=new Button("+");
+		btnsub=new Button("-");
+		btnmult=new Button("*");
+		btndiv=new Button("/");
+		btnreset=new Button("C");
+		btnexit=new Button("Exit");
+		
+		
+		pnl.add(lno1);
+		pnl.add(tno1);
+		pnl.add(lno2);
+		pnl.add(tno2);
+		pnl.add(lans);
+		pnl.add(tans);
+		
+		pnl.add(btnadd);
+		pnl.add(btnsub);
+		pnl.add(btnmult);
+		pnl.add(btndiv);
+		pnl.add(btnreset);
+		pnl.add(btnexit);
+		
+		
 	}
 }
