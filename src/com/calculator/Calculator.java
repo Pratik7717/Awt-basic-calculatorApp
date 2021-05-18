778150962cbd8ce382cad85c5a80709ccbcd91e8
Pratik Jagade <pratik.21810767@viit.ac.in>
2021-05-18 12:49:08 +0530

    Updates in Calculation class..Added the required data members

--- src/com/calculator/Calculator.java
+++ src/com/calculator/Calculator.java
@@ -5,6 +5,12 @@ import java.awt.*;
 public class Calculator {
 	Frame frm;
 	
+	Panel pnl;
+	
+	Label lno1,lno2,lans;
+	TextField tno1,tno2,tans;
+	Button btnadd,btnsub,btnmult,btndiv,btnreset,btnexit;
+	
 	public Calculator() {
 		frm=new Frame("Basic Calculator");
 		frm.setSize(400,400);
